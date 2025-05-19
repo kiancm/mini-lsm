deb762a7b49ba5f8d02d795df6b150efbd48714b
Kian Mehrabani <kianmehrabani@gmail.com>
2025-05-18 22:33:33 -0400

    implement week 1 day 2 task 4: read path - scan

--- mini-lsm-starter/src/lsm_storage.rs
+++ mini-lsm-starter/src/lsm_storage.rs
@@ -30,6 +30,7 @@ use crate::compact::{
     CompactionController, CompactionOptions, LeveledCompactionController, LeveledCompactionOptions,
     SimpleLeveledCompactionController, SimpleLeveledCompactionOptions, TieredCompactionController,
 };
+use crate::iterators::merge_iterator::MergeIterator;
 use crate::lsm_iterator::{FusedIterator, LsmIterator};
 use crate::manifest::Manifest;
 use crate::mem_table::MemTable;
@@ -383,9 +384,19 @@ impl LsmStorageInner {
     /// Create an iterator over a range of keys.
     pub fn scan(
         &self,
-        _lower: Bound<&[u8]>,
-        _upper: Bound<&[u8]>,
+        lower: Bound<&[u8]>,
+        upper: Bound<&[u8]>,
     ) -> Result<FusedIterator<LsmIterator>> {
-        unimplemented!()
+        let state = self.state.read();
+        let memtable_iters = std::iter::once(state.memtable.scan(lower, upper))
+            .chain(
+                state
+                    .imm_memtables
+                    .iter()
+                    .map(|mt| mt.as_ref().scan(lower, upper)),
+            )
+            .map(Box::new)
+            .collect();
+        LsmIterator::new(MergeIterator::create(memtable_iters)).map(FusedIterator::new)
     }
 }
